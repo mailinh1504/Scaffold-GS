88041b90a87f52a93db4af7e27a3753a09a91798
mailinh1504 <mailinh15042004@gmail.com>
2025-11-05 18:31:00 +0700

    add SymmetricalMLP

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -236,7 +236,7 @@ class SymmetricalMLP(nn.Module):
             out = self.last_act(out)
         return out
 
-Used_net = MultiplicativeNet
+Used_net = SymmetricalMLP
 
 # ============================================================================================
 
