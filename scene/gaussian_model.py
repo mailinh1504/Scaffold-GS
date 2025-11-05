709b35d9b2db1ff0638ae999615e35917286e724
mailinh1504 <mailinh15042004@gmail.com>
2025-11-05 14:02:49 +0700

    add AdditiveNet

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -236,7 +236,7 @@ class SymmetricalMLP(nn.Module):
             out = self.last_act(out)
         return out
 
-Used_net = LiteFiLMNet
+Used_net = AdditiveNet
 
 # ============================================================================================
 
